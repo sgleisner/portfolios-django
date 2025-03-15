f26dce0faf2fb54b336246b9b1f15bfd572aefac
Samuel Gleisner V <sgleisnerv@ripley.com>
2025-03-15 17:37:51 -0300

    chore: configure django admin

--- portfolios/admin.py
+++ portfolios/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Portfolio, Stock, StockPrice, Holding
 
-# Register your models here.
+admin.site.register(Stock)
+admin.site.register(StockPrice)
+admin.site.register(Holding)
+
+
+class HoldingInline(admin.TabularInline):
+    model = Holding
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    inlines = [HoldingInline]
