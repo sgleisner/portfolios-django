1ee4c0cfbcbe266e7fce771e95e4ac9ee754428e
Samuel Gleisner V <sgleisnerv@ripley.com>
2025-03-16 17:37:28 -0300

    feat: display errors in front when `profit` raises them

--- portfolios/views.py
+++ portfolios/views.py
@@ -21,30 +21,34 @@ class PortfolioDetailView(DetailView):
         if form.is_valid():
             start_date = form.cleaned_data["start_date"]
             end_date = form.cleaned_data["end_date"]
-            profit, annualized_return = self.object.profit(start_date, end_date)
-            results = {
-                "profit": profit,
-                "annualized_return": annualized_return * 100,  # as a percentage
-                "start_date": start_date,
-                "end_date": end_date,
-                "initial_value": self.object.value(start_date),
-                "final_value": self.object.value(end_date),
-                "days_held": (end_date - start_date).days,
-                "holdings_table": [
-                    {
-                        "symbol": holding.stock.symbol,
-                        "initial_price": holding.stock.price(start_date),
-                        "final_price": holding.stock.price(end_date),
-                        "quantity": holding.quantity,
-                        "profit": holding.quantity
-                        * (
-                            holding.stock.price(end_date)
-                            - holding.stock.price(start_date)
-                        ),
-                    }
-                    for holding in self.object.holdings.all()
-                ],
-            }
+
+            try:
+                profit, annualized_return = self.object.profit(start_date, end_date)
+                results = {
+                    "profit": profit,
+                    "annualized_return": annualized_return * 100,  # as a percentage
+                    "start_date": start_date,
+                    "end_date": end_date,
+                    "initial_value": self.object.value(start_date),
+                    "final_value": self.object.value(end_date),
+                    "days_held": (end_date - start_date).days,
+                    "holdings_table": [
+                        {
+                            "symbol": holding.stock.symbol,
+                            "initial_price": holding.stock.price(start_date),
+                            "final_price": holding.stock.price(end_date),
+                            "quantity": holding.quantity,
+                            "profit": holding.quantity
+                            * (
+                                holding.stock.price(end_date)
+                                - holding.stock.price(start_date)
+                            ),
+                        }
+                        for holding in self.object.holdings.all()
+                    ],
+                }
+            except Exception as e:
+                form.add_error(None, str(e))
 
         context["form"] = form
         context["results"] = results
